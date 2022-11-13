6bc11ee47debd945e0e32287daa401e7442c7a0c
Yash Gajanan Bansod <troy@pop-os.localdomain>
2022-11-13 11:02:10 +0530

    next-auth templete

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,48 @@
 import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import axios from 'axios';
 
-export default NextAuth({
-    providers: [],
-});
+export const authOptions = {
+    secret: process.env.JWT_SECRET,
+    session: {
+        strategy: 'jwt',
+    },
+    providers: [
+        CredentialsProvider({
+            name: 'credentials',
+            async authorize(credentials, req) {
+                const response = await axios
+                    .post('http://127.0.0.1:5000/api/login', {
+                        email: credentials.email,
+                        password: credentials.password,
+                    })
+                    .catch((err) => err.response);
+
+                if (response.status === 200) {
+                    return response.data;
+                }
+
+                return null;
+            },
+        }),
+    ],
+    pages: {
+        signIn: 'signin',
+    },
+    callbacks: {
+        async jwt({ token, user, account, profile, isNewUser }) {
+            if (user) {
+                token.id = user._id;
+                token.role = user.role;
+            }
+            return token;
+        },
+        async session({ session, user, token }) {
+            session.user.id = token.id;
+            session.user.role = token.role;
+            return session;
+        },
+    },
+};
+
+export default NextAuth(authOptions);
